ffaa413b81f945f2de05ce483252936f8f36ed54
conf0156 <conf0156@eeecs.qub.ac.uk>
2020-01-22 16:24:18 +0000

    ghghg

--- read_data_from_pdf.R
+++ read_data_from_pdf.R
@@ -4,6 +4,7 @@
 #
 
 install.packages("pdftools")
+install.packages("tabulizer")
 
 library("pdftools")
 library("glue")
@@ -28,10 +29,26 @@ table <- str_split(raw_text, "\r", simplify = TRUE) %>% str_split("\n", simplify
 View(table)
 write.csv(table, "table3.csv")
 
+
 raw_text <- pdf_text("report.pdf")
-cat(raw_text[c(49,50)])
-raw_text[41]
+cat(raw_text[c(70)])
+table <- str_split(raw_text[70], "\r\n", simplify = TRUE)
+view(table)
+
+out <- list()
+for (i in seq_along(table)){
+  out[[i]] <- str_split(table[i], "\\s{2,}", simplify = TRUE)
+}
+
+grep(pattern = "TABLE(.*)", table1)
+stringr::str_extract(pattern = "TABLE(.*)", raw_text[70])
+
+
 
 table <- str_split(raw_text, "\r", simplify = TRUE) %>% str_split("\n", simplify = TRUE) %>% str_split("          ", simplify = TRUE)
+view(table)
+
+
 View(table)
-write.csv(table, "table3.csv")
+View(table)\n
+write.csv(table, "
